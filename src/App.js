31c17ce172f21f4072d08d9e5e45841c35f20db2
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-09-23 19:20:55 -0300

    feat: game start

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import img4 from "./assets/forca4.png";
 import img5 from "./assets/forca5.png";
 import img6 from "./assets/forca6.png";
 
+import words from "./Words"
+
 const images = [img0, img1, img2, img3, img4, img5, img6];
 
 const letters = [
@@ -20,18 +22,65 @@ const letters = [
 
 let i = 0;
 
+let novoArray = [];
+
 export default function App() {
     const [disabled, setDisabled] = useState(false);
     const [disabledInput, setDisabledInput] = useState(true);
     const [disabledButton, setDisabledButton] = useState(true);
     const [hangman, setHangman] = useState(images[i]);
+    const [word, setWord] = useState([]);
+    const [hideWord, setHideWord] = useState([]);
+
+    function escolhaPalavra() {
+        setDisabled(true);
+        setDisabledInput(false);
+        setDisabledButton(false);
+
+        const randomElement = words[Math.floor(Math.random() * words.length)];
+        console.log(randomElement);
+        setWord(randomElement.split(""));
+     
+        for(let i = 0; i < randomElement.length; i++) {
+            novoArray.push('_');
+        }
+
+        console.log(novoArray.length);
+        setHideWord(novoArray);        
+    }
+
+    // function escolhaPalavra() {
+    //     if (i == 5) {
+    //         setDisabled(true);
+    //         setTimeout(() => {
+    //             alert("Perdeu");
+    //         }, 200)
+    //     }
+
+    //     i++;
+    //     setHangman(images[i]);
+    // }    
+
+    console.log(hideWord);
+    console.log(novoArray);
+    console.log(word);
 
     return (
         <div className="container">
             <div className="gameScreen">
-                <img src={hangman} alt="vaiCurintia 🦅" />
+                <img src={hangman} alt="vaiCurintia :eagle:" />
 
-                <button disabled={disabled}>Escolher palavra</button>
+                <button onClick={escolhaPalavra} disabled={disabled}>Escolher palavra</button>
+
+                <div className="choosenWord">
+                    {
+                        hideWord.map((word, index) => {
+                            return <div key={index}>
+                                <h1>{word.toUpperCase()}</h1>
+                            </div>
+                        })
+                    }
+                </div>
             </div>
 
             <div className="keyboardScreen">
@@ -47,7 +96,7 @@ export default function App() {
 
                 <div className="guessWord">
                     Já sei a palavra!
-                    <input type="text" placeholder="Escreva aqui..." disabled={disabledInput}></input>
+                    <input type="text" placeholder="Escreva aqui..." onChange={() => console.log("Mudou o valor!")} disabled={disabledInput}></input>
                     <button>Chutar</button>
                 </div>
             </div>
